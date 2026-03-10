862e8e4704da704dc3017323510a9b6e753df68a
Eswar <g.eswarsairam7@gmail.com>
2026-03-10 08:46:11 +0530

    Initial commit - FSAD Skill Exam

---
+++ src/main/java/com/klef/fsad/exam/controller/ShipmentController.java
@@ -0,0 +1,27 @@
+package com.klef.fsad.exam.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.klef.fsad.exam.model.Shipment;
+import com.klef.fsad.exam.service.ShipmentService;
+
+@RestController
+@RequestMapping("/shipment")
+public class ShipmentController 
+{
+    @Autowired
+    ShipmentService service;
+
+    @PostMapping("/add")
+    public Shipment addShipment(@RequestBody Shipment s)
+    {
+        return service.addShipment(s);
+    }
+
+    @PutMapping("/update/{id}")
+    public Shipment updateShipment(@PathVariable Long id, @RequestBody Shipment s)
+    {
+        return service.updateShipment(id, s);
+    }
+}
